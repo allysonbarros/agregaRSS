4a4d2daff5400bc575cf8546f2b672fea571d689
Allyson Barros <allysonbarrosrn@gmail.com>
2012-11-20 11:39:54 -0300

    Realizando a validação dos campos.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+
+  validates :name, :description, :presence => {:message => "deve ser preenchido."}
   
   attr_accessible :name, :description
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,5 +2,7 @@ class Feed < ActiveRecord::Base
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   has_one :category, :class_name => "Category", :foreign_key => "category_id"
   
+  validates :name, :description, :url, :presence => {:message => "deve ser preenchido."}
+  
   attr_accessible :name, :description, :url
 end
